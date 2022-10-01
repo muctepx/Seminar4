d05101c82c284f5e523b116355fdb2c90b2b6b41
daniil <daniil.chuprin@gmail.com>
2022-10-01 19:53:20 +0300

    addtext

---
+++ Задача25/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25:
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    }
+    Console.WriteLine(result);
+}
+
+
+int ReadData(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+var a = ReadData("Введите число a: ");
+var b = ReadData("Введите число b: ");
+ToDegree(a, b);
---
+++ Задача29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 
+// элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+string[] PrintArray(string arrayStr)
+{
+    string[] array = arrayStr.Split(", ");
+    return array;
+}
+Console.WriteLine("Введите массив из 8 числе через запятую: ");
+string arrayStr = Console.ReadLine()??"";
+int arrLenght = arrayStr.Length;
+string [] array = PrintArray(arrayStr);
+for (var i=0; i < array.Length; i++)
+{
+   Console.Write($"{arrayStr} -> [{array[i]}," + "]");
+}
